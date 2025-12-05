dc0b00c4a43ec514c99635df4e7e80e265d13596
kaster <diogokaster@gmail.com>
2025-12-04 22:05:40 -0300

    feat: create Order module service provider

---
+++ modules/Order/Providers/OrderServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Order\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class OrderServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    }
+}
